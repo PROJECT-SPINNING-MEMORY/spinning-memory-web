7523eb9381e5188524280243747e5c3deb348d0c
Vaidehi Patel <patel.4526@buckeyemail.osu.edu>
2025-11-20 17:50:57 -0500

    added li key product id

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -29,7 +29,7 @@ function Products() {
         <div className="content">
             <ul className="products">
             {products.map((product) => (
-                <li>
+                <li key={product.id}>
                     <div className="product">
                         <img
                             className="product-image"
